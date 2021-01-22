874b30b2fbc48084b5b5df4514795d2af98adf5c
Taslemun Nahar <taslemun@gmail.com>
2021-01-21 22:38:44 -0500

    updating

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -38,12 +38,35 @@ function showData() {
         }];
 
         Plotly.newPlot('bubble', bubbleData);
+
+        // BAR
+
+        d3.json("/samples").then((sdata) => {
+            subject_id = selection;
+            var samples = sdata['samples'];
+            var filtered_samples = samples.filter(s => s['id'] == subject_id);
+            var sample_values = filtered_samples.map(s => s['sample_values']);
+            var otu_ids = filtered_samples.map(s => s['otu_ids']);
+            var otu_labels = ''
+            var x = sample_values[0].slice(0,10).reverse();
+            var y = otu_ids[0].slice(0,10).map(o => `OTU ${o}`).reverse();
+            var data = [{
+              type: 'bar',
+              x: x,
+              y: y,
+              orientation: 'h'
+            }];
+            
+            Plotly.newPlot('bar', data);
+        });
+
     });
-};
+    
+
+}
 
 function optionChanged() {
     d3.select('select').on('change', function () {
         showData();
     })
 }
-
